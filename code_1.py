ee8ffc347b06b188148da20f1e07eef5085afb41
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-03 19:22:15 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,40 +1,66 @@
-def main():
-
-import random
     data = [random.randint(1, 100) for _ in range(10)]
-
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-    return data
-    main()
-    for item in data:
+def main():
 if __name__ == "__main__":
-def generate_random_data():
-
     data = generate_random_data()
-
-def generate_random_data():
-if __name__ == "__main__":
-    for item in data:
-    main()
 import random
     return data
 
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-        print(f"Random Number: {item}")
-import random
+    main()
 def generate_random_data():
-def main():
+
+    for item in data:
+        print(f"Random Number: {item}")
+
+    for item in data:
     return data
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
     data = generate_random_data()
+import random
 
     main()
-        print(f"Random Number: {item}")
-    for item in data:
-if __name__ == "__main__":
 
+
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+def main():import random
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    main()
+def generate_random_data():
+if __name__ == "__main__":
+def main():
+    data = generate_random_data()
+
+
+    return data
+    data = generate_random_data()
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+
+
+def generate_random_data():
+    for item in data:
+        print(f"Random Number: {item}")
+
+    return data
+if __name__ == "__main__":
+import randomimport random
+    main()
 
     data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def generate_random_data():
+    for item in data:
+
+def main():
+
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
