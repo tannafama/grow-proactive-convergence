6204568049ecbec7742194a8aaf5382580f83e73
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-03 17:53:13 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,40 @@
+def main():
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    return data
+    main()
+    for item in data:
+if __name__ == "__main__":
+def generate_random_data():
+
+    data = generate_random_data()
+
+def generate_random_data():
+if __name__ == "__main__":
+    for item in data:
+    main()
+import random
+    return data
+
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+        print(f"Random Number: {item}")
+import random
+def generate_random_data():
+def main():
+    return data
+    data = generate_random_data()
+
+    main()
+        print(f"Random Number: {item}")
+    for item in data:
+if __name__ == "__main__":
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
